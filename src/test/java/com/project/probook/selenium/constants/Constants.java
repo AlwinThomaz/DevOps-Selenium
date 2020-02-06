6abfd4b680b16627c56baa29fa2fd8b177e19071
alwin <alwinthomasgt@outlook.com>
2020-02-06 06:26:46 -0500

    ip

--- src/test/java/com/project/probook/selenium/constants/Constants.java
+++ src/test/java/com/project/probook/selenium/constants/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	public static final String PATH = "chromedriver";
 	public static final String PROPERTY = "webdriver.chrome.driver";
 	
-	public static final String HOST = "ec2-3-10-227-78.eu-west-2.compute.amazonaws.com";
+	public static final String HOST = "http://ec2-3-10-227-78.eu-west-2.compute.amazonaws.com";
 	//public static final String HOST = "http://localhost:8080/proBook";
 	
 	public static final String INDEX = "/index.html";
